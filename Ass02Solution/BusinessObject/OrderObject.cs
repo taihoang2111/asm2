cb5fd814d96de0bec81c2c329037bb6f7ebaf961
taihoang2111 <134487386+taihoang2111@users.noreply.github.com>
2023-07-03 12:24:58 +0700

    Add files via upload

---
+++ Ass02Solution/BusinessObject/OrderObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class OrderObject
+    {
+        public int OrderID { get; set; }
+        public int MemberID { get; set; }
+        public DateTime Orderdate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedDate     { get; set; }
+        public decimal Freight { get; set; }
+    }
+}
---
+++ Ass02Solution/DataAccess/MemberDAO.cs
@@ -0,0 +1,27 @@
+﻿using BusinessObject;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess
+{
+    public class MemberDAO : DbContext
+    {
+        public DbSet<MemberObject> Members { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MySaleDB"));
+        }
+       
+    }
+}
